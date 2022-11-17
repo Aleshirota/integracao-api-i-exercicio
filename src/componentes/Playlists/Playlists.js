21481f3600ad01c81634860353319d2b9195f3f2
Shiro <aleshirota@gmail.com>
2022-11-17 19:05:50 -0300

    integracao-api-concluido

--- src/componentes/Playlists/Playlists.js
+++ src/componentes/Playlists/Playlists.js
@@ -1,31 +1,54 @@
-import React, {  useState } from "react";
+import React, {  useState, useEffect } from "react";
 import Musicas from "../Musicas/Musicas";
+import axios from "axios"
 
-const playlistsLocal = [
-    {
-        id: 1,
-        name: "Playlist 1"
-    },
-    {
-        id: 2,
-        name: "Playlist 2"
-    },
-    {
-        id: 3,
-        name: "Playlist 3"
-    },
-    {
-        id: 4,
-        name: "Playlist 4"
-    },
-]
 function Playlists() {
-    const [playlists, setPlaylists] = useState(playlistsLocal)
+    const [playlists, setPlaylists] = useState([])
+    const [nome, setNome] = useState("")
+
+    const getAllPlaylists = () =>{
+        axios.get("https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists", {
+          headers:{
+            Authorization: "Shirota-ammalA"
+          }
+        })
+        .then((resposta)=>{
+          setPlaylists(resposta.data.result.list)
+        })
+        .catch((erro)=>{
+          console.log(erro)
+        })
+      }
+      useEffect(()=>{
+        getAllPlaylists()
+      },[])
+    
+    const criaPlaylist = ()=>{
+         const body = {
+             name: nome
+         }
+
+         const headers= {
+             headers:{
+                 Authorization: "Shirota-ammalA"
+             }
+         }
+
+         axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists", body, headers)
+         .then((resposta)=>{
+            console.log(resposta)
+         })
+         .catch((erro)=>{
+            console.log(erro)
+         })
+     }
   
     return (
         <div>
             {playlists.map((playlist) => {
-                return <Musicas key={playlist.id} playlist={playlist}/>
+                return <Musicas 
+                key={playlist.id} 
+                playlist={playlist}/>
             })}
 
         </div>
